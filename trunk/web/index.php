ce0cc4aa184b7b7efaa37d7e2f1f8ff093456db4
xcojack <xcojack@912e26ff-9329-95ef-fba8-717cf07248fb>
2010-09-16 08:06:53 +0000

    Inicjalizacja pliku index.php oraz pliku .htaccess niezbędnych do działania systemu

---
+++ trunk/web/index.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ *  Basic PHP Develop Tools (BDT)
+ *  Copyright (C) 2010 Aichra.pl
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @author     Przemysław Czekaj <przemyslaw.czekaj@aichra.pl>
+ * @link       http://aichra.pl
+ * @version    0.1
+ * @since      23.03.2010
+ * @package    BDT
+ * @subpackage web
+ * @charset    utf8
+ **/
+
+require_once('./../lib/BDT/BDT_Bootstrap.php');
+
+class Bootstrap extends BDT_Bootstrap {
+
+   /**
+    * Pseoud inicjalizator klasy
+    *
+    * @param   void
+    * @access  protected
+    * @return  void
+    */
+   public function __construct() {
+
+      /**
+       * Typ środowiska w którym się poruszamy
+       *
+       * @var  bool  TRUE     developerskie ( debugger ( wyświetlanie błędów ), brak logowania błędów )
+       * @var  bool  FALSE    produkcyjne ( brak wyświetlania błędów, logowanie błędów do db )
+       */
+      $this->_environment = TRUE;
+
+      parent::__construct();
+   }
+}
+
+/**
+ * Żeby się zbytnio nie bawić, wywołujemy klasę raz dwa trzy
+ */
+new Bootstrap;
